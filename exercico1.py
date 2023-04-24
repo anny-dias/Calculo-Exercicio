496e01e27d6d028b15db8d9b638842142e736f47
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-24 09:40:55 -0300

    calculo

--- exercico1.py
+++ exercico1.py
@@ -1,6 +1,6 @@
 inicial = 10
 mensal = 5
-mes = 5
+mes = 24
 taxa = 1.01
 soma = 0
 t = 0 
@@ -12,8 +12,3 @@ while t < mes:
 valor = inicial * taxa ** mes + mensal * soma
 print(f"{valor}")
 
-while mes < 24:
-    valor = 1.01 ** mes
-    soma += valor
-    mes+= 1 
-print(f"{valor}")
